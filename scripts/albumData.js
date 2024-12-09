c99acaaf3779c1bb30c2efe527f5e697e5d94bac
Zi Di <grosswamp@gmail.com>
2024-12-09 18:46:55 -0500

    uploaded other tracks

--- scripts/albumData.js
+++ scripts/albumData.js
@@ -2,11 +2,19 @@ const Dec5th = {
     title: "December 5th",
     cover:"albums/pubCrawl/pubcrawl.png",
     songs: [
-        { name: "A Midnight Mystery", src: "albums/dec5th/audio/aMidnightMistery.wav"},
-        { name: "Bad Childhood", src: "albums/dec5th/audio/badChildhood.wav"},
-        { name: "Bella the Sleepy Cat", src: "albums/dec5th/audio/BellaTheSleepyCat.wav"},
-        { name: "Starry Sky", src: "albums/dec5th/audio/StarrySky.wav"},
-        { name: "Sun Room", src: "albums/dec5th/audio/SunRoom.wav"},
+        { name: "aMidnightMystery", src: "albums/dec5th/audio/aMidnightMistery.wav"},
+        { name: "badChildhood", src: "albums/dec5th/audio/badChildhood.wav"},
+        { name: "BellaTheSleepyCat/", src: "albums/dec5th/audio/BellaTheSleepyCat.wav"},
+        { name: "blesser", src: "albums/dec5th/audio/blesser.wav"},
+        { name: "catheaebve", src: "albums/dec5th/audio/catheaebve.wav"},
+        { name: "chalkonthegorillaglass", src: "albums/dec5th/audio/chalkonthegorillaglass.wav"},
+        { name: "computerstym", src: "albums/dec5th/audio/computerstym.wav"},
+        { name: "/Dec5th/", src:"albums/dec5th/audio/Dec5th.wav"},
+        { name: "horizonagain", src: "albums/dec5th/audio/horizonagain.wav"},
+        { name: "jam_ihavelyricsinmyhead", src:"albums/dec5th/audio/jam_ihavelyricsinmyhead.wav"},
+        { name: "shitasshead", src: "albums/dec5th/audio/shitasshead.wav"},
+        { name: "/SunRoom", src: "albums/dec5th/audio/SunRoom.wav"},
+        { name: "StarrySky", src: "albums/dec5th/audio/StarrySky.wav"},
         { name: "Wasted", src: "albums/dec5th/audio/Wasted.wav"}
     ]
 };
