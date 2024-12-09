f578edfdbd5f9ce87deae2e6d5d3ed6dc17f5c8a
Zi Di <grosswamp@gmail.com>
2024-12-09 16:57:13 -0500

    uploaded dec 5th album

---
+++ scripts/albumData.js
@@ -0,0 +1,13 @@
+const Dec5th = {
+    title: "December 5th",
+    cover:"albums/pubCrawl/pubcrawl.png",
+    songs: [
+        { name: "A Midnight Mystery", src: "albums/dec5th/audio/aMidnightMistery.wav"},
+        { name: "Bad Childhood", src: "albums/dec5th/audio/badChildhood.wav"},
+        { name: "Bella the Sleepy Cat", src: "albums/dec5th/audio/BellaTheSleepyCat.wav"},
+        { name: "Starry Sky", src: "albums/dec5th/audio/StarrySky.wav"},
+        { name: "Sun Room", src: "albums/dec5th/audio/SunRoom.wav"},
+        { name: "Wasted", src: "albums/dec5th/audio/Wasted.wav"}
+    ]
+};
+
