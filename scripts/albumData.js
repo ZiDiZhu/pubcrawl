0785926ffd820f6ce0ab9095bd373b64989ffe91
Zi Di <grosswamp@gmail.com>
2025-01-30 15:09:57 -0500

    adding more tracks

--- scripts/albumData.js
+++ scripts/albumData.js
@@ -20,3 +20,39 @@ const Dec5th = {
     ]
 };
 
+const misc_pub = {
+    title: "Miscellaneous Pub",
+    cover:"albums/pubCrawl/pubcrawl.png",
+    songs: [
+        { name: "9-11_demonovocalsv1.wav", src: "albums/misc_pub/9-11_demonovocalsv1.wav"},
+        { name: "Cripple Creek.wav", src: "albums/misc_pub/Cripple Creek.wav"},
+        { name: "fingernailcrud_demo0.2.wav", src: "albums/misc_pub/fingernailcrud_demo0.2.wav"},
+        { name: "typscrep.wav", src: "albums/misc_pub/typscrep.wav"},
+        { name: "woopdoo_EARLYvocldemo0.wav", src: "albums/misc_pub/woopdoo_EARLYvocldemo0.wav"}
+    ]
+};
+
+
+const misc_crawl = {
+    title: "December 5th",
+    cover:"albums/pubCrawl/pubcrawl.png",
+    songs: [
+        { name: "aMidnightMystery", src: "albums/dec5th/audio/aMidnightMistery.wav"},
+        { name: "badChildhood", src: "albums/dec5th/audio/badChildhood.wav"},
+        { name: "BellaTheSleepyCat/", src: "albums/dec5th/audio/BellaTheSleepyCat.wav"},
+        { name: "blesser", src: "albums/dec5th/audio/blesser.wav"},
+        { name: "catheaebve", src: "albums/dec5th/audio/catheaebve.wav"},
+        { name: "chalkonthegorillaglass", src: "albums/dec5th/audio/chalkonthegorillaglass.wav"},
+        { name: "computerstym", src: "albums/dec5th/audio/computerstym.wav"},
+        { name: "/Dec5th/", src:"albums/dec5th/audio/Dec5th.wav"},
+        { name: "grop", src: "albums/dec5th/audio/grop.wav"},
+        { name: "horizonagain", src: "albums/dec5th/audio/horizonagain.wav"},
+        { name: "jam_ihavelyricsinmyhead", src:"albums/dec5th/audio/jam_ihavelyricsinmyhead.wav"},
+        { name: "shitasshead", src: "albums/dec5th/audio/shitasshead.wav"},
+        { name: "/SunRoom", src: "albums/dec5th/audio/SunRoom.wav"},
+        { name: "StarrySky", src: "albums/dec5th/audio/StarrySky.wav"},
+        { name: "Wasted", src: "albums/dec5th/audio/Wasted.wav"}
+    ]
+};
+
+
